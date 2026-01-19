20021825bfffd0ddd05fe8e3bb1b71f8c0a3d36d
Rafif.s <rafifsudanta1@gmail.com>
2026-01-19 18:00:12 +0700

    Revert "Update index.ts"
    This reverts commit a0c88480511920a4a67bcf3c0ccb80391527109a.

--- app/components/wizard/index.ts
+++ app/components/wizard/index.ts
@@ -1,5 +1,5 @@
 // Wizard components barrel export
-export { WizardProgress } from './WizardProgress';
+export { WizardProgress, WizardProgressCompact } from './WizardProgress';
 export { WizardNavigation, WizardNavigationSticky } from './WizardNavigation';
 export { ISDCTree } from './ISDCTree';
 export type { ISDCSelection } from './ISDCTree';
